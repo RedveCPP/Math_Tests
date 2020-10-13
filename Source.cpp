82eba00398cec93326dcbc18666d88609f7b6cc6
redve <proninja234@gmail.com>
2020-10-13 16:12:51 +0200

    grerg

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,51 @@
 #include <glm/vec2.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
 #include <iostream>
+
+class Shape
+{
+protected:
+	glm::vec2 position;
+public:
+	virtual double GetArea() = 0;
+	virtual double GetDiameter() = 0;
+};
+
+class Circle :public Shape
+{
+private:
+	double radius;
+public:
+	Circle(glm::vec2&& position, double radius)
+	{
+		this->position = position;
+		this->radius = radius;
+	}
+	virtual double GetArea()
+	{
+		return glm::pi<float>() * glm::pow(radius, 2);
+	}
+
+	virtual double GetDiameter()
+	{
+		return glm::pi<float>() * radius * 2;
+	}
+};
+
+double GetArea(Shape& shape)
+{
+	return shape.GetArea();
+}
+
+double GetDiameter(Shape& shape)
+{
+	return shape.GetDiameter();
+}
 
 int main()
 {
-	glm::vec2 vector1(400,300);
-	glm::vec2 vector2(400,300);
-	vector1 += vector2;
-	std::cout << vector1.x;
+	Circle circle(glm::vec2(400, 300), 5);
+	std::cout << GetArea(circle);
 	return 0;
 }
