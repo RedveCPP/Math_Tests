234174c4df561d23a83085b872f0884059308296
redve <proninja234@gmail.com>
2020-10-13 19:10:28 +0200

    gg

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,20 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 class Shape
 {
 protected:
-	glm::vec2 position;
+	glm::vec2 _position;
 public:
+
+	glm::vec2 GetPosition()
+	{
+		return _position;
+	}
+
+	virtual void Draw(sf::RenderWindow& window, sf::Color color = sf::Color::White) = 0;
 	virtual double GetArea() = 0;
 	virtual double GetDiameter() = 0;
 };
@@ -15,37 +23,63 @@ public:
 class Circle :public Shape
 {
 private:
-	double radius;
+	double _radius;
 public:
-	Circle(glm::vec2&& position, double radius)
+	Circle(glm::vec2 position, double radius) :
+		_radius{ radius }
 	{
-		this->position = position;
-		this->radius = radius;
+		_position = position;
 	}
+
+	virtual void Draw(sf::RenderWindow& window, sf::Color color = sf::Color::White)
+	{
+		sf::CircleShape circle(_radius);
+		circle.setPointCount(1000);
+		circle.setFillColor(color);
+		circle.setPosition(_position.x - _radius, _position.y - _radius);
+		window.draw(circle);
+	}
+
 	virtual double GetArea()
 	{
-		return glm::pi<float>() * glm::pow(radius, 2);
+		return glm::pi<double>() * glm::pow(_radius, 2);
 	}
 
 	virtual double GetDiameter()
 	{
-		return glm::pi<float>() * radius * 2;
+		return glm::pi<double>() * _radius * 2;
+	}
+
+	double GetRadius()
+	{
+		return _radius;
 	}
 };
 
-double GetArea(Shape& shape)
-{
-	return shape.GetArea();
-}
-
-double GetDiameter(Shape& shape)
-{
-	return shape.GetDiameter();
-}
-
 int main()
 {
-	Circle circle(glm::vec2(400, 300), 5);
-	std::cout << GetArea(circle);
+	Circle circle1(glm::vec2(400, 300), 100);
+	Circle circle2(glm::vec2(600, 300), 100);
+	if (glm::distance(circle1.GetPosition(), circle2.GetPosition()) <= circle1.GetRadius() + circle2.GetRadius())
+	{
+		std::cout << "Circle collision detected!" << std::endl;
+	}
+
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "sfml");
+	sf::Event event;
+	while (window.isOpen())
+	{
+		window.clear();
+		while (window.pollEvent(event))
+		{
+			if (event.type == event.Closed)
+			{
+				window.close();
+			}
+		}
+		circle1.Draw(window);
+		circle2.Draw(window, sf::Color::Red);
+		window.display();
+	}
 	return 0;
 }
